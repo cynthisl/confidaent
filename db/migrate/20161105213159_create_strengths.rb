b2aa18f1c7bcdb6cb6106d48b4e94c8f347f16ad
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-11-05 14:57:28 -0700

    strength and request migration data

--- db/migrate/20161105213159_create_strengths.rb
+++ db/migrate/20161105213159_create_strengths.rb
@@ -1,7 +1,8 @@
 class CreateStrengths < ActiveRecord::Migration
   def change
     create_table :strengths do |t|
-
+      t.belongs_to :skill
+      t.belongs_to :user
       t.timestamps null: false
     end
   end
--- db/migrate/20161105213234_create_requests.rb
+++ db/migrate/20161105213234_create_requests.rb
@@ -1,7 +1,8 @@
 class CreateRequests < ActiveRecord::Migration
   def change
     create_table :requests do |t|
-
+      t.belongs_to :skill
+      t.belongs_to :user
       t.timestamps null: false
     end
   end
