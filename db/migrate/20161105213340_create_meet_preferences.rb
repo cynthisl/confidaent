3691aa88ad7b0371da13aca41721967c1014e5ad
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-11-05 14:59:35 -0700

    meet preferences migration data fields

--- db/migrate/20161105213340_create_meet_preferences.rb
+++ db/migrate/20161105213340_create_meet_preferences.rb
@@ -1,7 +1,8 @@
 class CreateMeetPreferences < ActiveRecord::Migration
   def change
     create_table :meet_preferences do |t|
-
+      t.belongs_to :user
+      t.string :meet_type
 
       t.timestamps null: false
     end
