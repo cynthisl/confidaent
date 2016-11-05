88ece399f84f8fe45a384b15ab87b3f547239b0e
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-11-05 14:51:52 -0700

    user migration data

--- db/migrate/20161105213147_create_users.rb
+++ db/migrate/20161105213147_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.integer :zip_code
+      t.datetime :birthdate
+      t.integer :career_status
+      
       t.timestamps null: false
     end
   end
