6312722596312b28695fa8af7ce8c61b426a26cb
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-11-05 15:07:57 -0700

    meet over migration data

--- db/migrate/20161105213348_create_meet_overs.rb
+++ db/migrate/20161105213348_create_meet_overs.rb
@@ -1,7 +1,10 @@
 class CreateMeetOvers < ActiveRecord::Migration
   def change
     create_table :meet_overs do |t|
-
+      t.boolean :food
+      t.boolean :drink
+      t.boolean :coffee
+      t.belongs_to :meet_preference
       t.timestamps null: false
     end
   end
