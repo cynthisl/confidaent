ba667437ae35cc6a9c9d2e608c5a062fd21c9fd5
cynthia <cynthisl@users.noreply.github.com>
2016-11-05 22:56:07 -0700

    Implemented User create to db, show User page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,20 @@
 class UserController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+    end
+
     def new
     end
+
+    def create
+        @user = User.new(user_params)
+
+        @user.save
+        redirect_to @user
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:first_name, :last_name, :zip_code, :birthdate, :career_status)
+        end
 end
