1251d21124bbf3f2875ca2a3cb89611a44b06ba6
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-11-05 23:07:23 -0700

    fix skill associations to user

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,6 +2,6 @@ class Skill < ActiveRecord::Base
   has_many :strengths
   has_many :requests
 
-  has_many :users, :through => :strength
-  has_many :users, :through => :request
+  has_many :users, :through => :strengths
+  # has_many :users, :through => :requests
 end
